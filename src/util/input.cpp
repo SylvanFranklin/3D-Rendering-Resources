0b7783a60716db62bebb5b4a8153d22510087c9b
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-14 11:16:34 -0500

    fix import glm

--- src/util/input.cpp
+++ src/util/input.cpp
@@ -1,4 +1,6 @@
 #include "../engine.h"
+#include <glm/glm.hpp>
+using namespace glm;
 
 void Engine::processInput() {
 	glfwPollEvents();
@@ -7,10 +9,26 @@ void Engine::processInput() {
 	if (input->key_pressed(GLFW_KEY_Q))
 		glfwSetWindowShouldClose(window, true);
 
-
 	if (input->key_pressed(GLFW_KEY_P))
 		model *= 1.2;
 
 	if (input->key_pressed(GLFW_KEY_M))
 		model *= 0.8;
+
+	vec3 direction = vec3(0.0);
+	if (input->key_pressed(GLFW_KEY_UP))
+		direction += vec3(0.0f, 0.0f, 0.2f);
+	if (input->key_pressed(GLFW_KEY_DOWN))
+		direction += vec3(0.0f, 0.0f, -0.2f);
+	if (input->key_pressed(GLFW_KEY_LEFT))
+		direction += vec3(-0.2f, 0.0f, 0.0f);
+	if (input->key_pressed(GLFW_KEY_RIGHT))
+		direction += vec3(0.2f, 0.0f, 0.0f);
+	if (input->key_pressed(GLFW_KEY_SPACE))
+		direction += vec3(0.0f, 0.2f, 0.0f);
+	if (input->key_pressed(GLFW_KEY_LEFT_SHIFT))
+		direction += vec3(0.0f, -0.2f, 0.0f);
+
+	camera->ProcessMouseMovement(this->input->getMouseXOffset(),
+								 this->input->getMouseYOffset());
 }
