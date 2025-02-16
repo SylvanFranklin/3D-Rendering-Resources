c44c0b4c56b77b823b9a0f4d4272c00d42a838e2
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-16 15:22:01 -0500

    feat: file split and made methods consistent

--- src/util/inputHandler.hpp
+++ src/util/inputHandler.hpp
@@ -4,48 +4,23 @@
 #include <GLFW/glfw3.h>
 #include <algorithm>
 #include <iterator>
+#include <glm/glm.hpp>
 
 class InputHandler {
 
   public:
-	InputHandler() {};
-	// InputHandler(GLFWwindow *window) : window(window) {};
-
-	void update_mouse(GLFWwindow *window) {
-		double mouseX, mouseY;
-		glfwGetCursorPos(window, &mouseX, &mouseY);
-
-		// we do this to not stap the camera around
-		if (this->firstMouse) {
-			lastMouseX = mouseX;
-			lastMouseY = mouseY;
-			this->firstMouse = false;
-		}
-		MouseXOffset = mouseX - lastMouseX;
-		MouseYOffset = lastMouseY - mouseY;
-		lastMouseX = mouseX;
-		lastMouseY = mouseY;
-	}
-
-	void update_keys(GLFWwindow *window) {
-		std::copy(std::begin(keys), std::end(keys), std::begin(last_keys));
-		for (int key = 0; key < 1024; ++key) {
-			if (glfwGetKey(window, key) == GLFW_PRESS)
-				keys[key] = true;
-			else if (glfwGetKey(window, key) == GLFW_RELEASE)
-				keys[key] = false;
-		}
-	};
-
+	InputHandler(GLFWwindow *window);
+	void update_mouse();
+	void update_keys();
 	bool key_pressed(int key) { return keys[key]; }
 	bool key_pressed_released(int key) { return !keys[key] && last_keys[key]; }
-	float getMouseXOffset() { return MouseXOffset; }
-	float getMouseYOffset() { return MouseYOffset; }
+	glm::vec2 get_mouse_pos();
 
   private:
 	bool keys[1024];
 	bool last_keys[1024];
 	double lastMouseX, lastMouseY, MouseXOffset, MouseYOffset;
+	GLFWwindow *window;
 	bool firstMouse = true;
 };
 
